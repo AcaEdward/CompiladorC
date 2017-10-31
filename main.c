776137d4c28aeef5104a0c88ba0f857dc4273be9
Eduardo <adventurehunter.dmd@gmail.com>
2017-10-31 10:29:05 -0200

    lexico modificado para uma função recursiva

--- main.c
+++ main.c
@@ -2,45 +2,48 @@
 #include <stdlib.h>
 #include <string.h>
 
-int lexico();
+int lexico(FILE *arq,char *ch2,	char ch,char I );
 char url[]="test.txt";
+int num = 0;
+int i = 0;
 char path[]="test.txt";
 char literalsTable[10][10] = {"programa", "leia", "escreva ","se", "senao", "para", "inteiro", "caractere","real", "fim"};
+//char identificadores = { ""}
 
 int main(void)
-{
-	
-	lexico();
+{	FILE *arq;
+	char *ch2 = malloc(1);
+	char ch;
+	arq = fopen(url, "r");
+	lexico(arq,ch2,ch);
+	fclose(arq);
 	return 0;
 
 
 }
-int lexico(){
-	char ch;
-	char *ch2 =  malloc(1);
-	FILE *arq;
-	int num = 0;
-	int i = 0;
+int lexico(FILE *arq,char *ch2,	char ch,char I ){
+	
 	int j =0;
-	arq = fopen(url, "r");
+	
 	if(arq == NULL)
 	    printf("Erro, nao foi possivel abrir o arquivo\n");
 	else
-	    while( (ch=fgetc(arq))!= EOF ){
+	    if( (ch=fgetc(arq))!= EOF ){
+	    	if((112=ch)&&(i==0)){
+	    		lexico(arq,ch2,ch,I);
+			}
 			if(ch == '\n')
 			  	printf("\t |%i| ", ++num);
 			//putchar(ch);
 			printf("%c", ch);
 			ch2[i++] = ch;
 			ch2 = realloc(ch2, i+1);
+			lexico(arq,ch2,ch);
 		}
-		while( j<i ){
+		/*while( j<i ){
 			printf("%c", ch2[j++]);
-		}
-		printf("Existem %d linhas no arquivo\n", num);
-	
-			
-	fclose(arq);
-	
+		}*/
+		
+		//printf("Existem %d linhas no arquivo\n", num);
 	return 0;
 }
