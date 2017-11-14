4ea4db96bd2a7de2f682bff616627417fa6f91b7
Eduardo <AcaEdward@users.noreply.github.com>
2017-11-14 10:18:14 -0200

    estudo de manipulação de ponteiros

--- main.c
+++ main.c
@@ -2,48 +2,65 @@
 #include <stdlib.h>
 #include <string.h>
 
-int lexico(FILE *arq,char *ch2,	char ch,char I );
+void lexico(FILE *arq);
+//char *ch2 = malloc(4);
 char url[]="test.txt";
+char *c;
 int num = 0;
 int i = 0;
+int il = 0;
+int lineCount =0;
+int lineSizeCount;
 char path[]="test.txt";
 char literalsTable[10][10] = {"programa", "leia", "escreva ","se", "senao", "para", "inteiro", "caractere","real", "fim"};
 //char identificadores = { ""}
 
 int main(void)
 {	FILE *arq;
-	char *ch2 = malloc(1);
+	char *ch2 ;//= malloc(1);
 	char ch;
 	arq = fopen(url, "r");
-	lexico(arq,ch2,ch);
+	lexico(arq);
 	fclose(arq);
+	int j;
+//	for(j=0;j<i;j++){
+//		printf("%c",c[j]);
+//	}
 	return 0;
 
-
 }
-int lexico(FILE *arq,char *ch2,	char ch,char I ){
-	
+void lexico(FILE *arq){
+	char *ch2 =malloc(1);
+	int v;
 	int j =0;
-	
+	char ch;
 	if(arq == NULL)
 	    printf("Erro, nao foi possivel abrir o arquivo\n");
 	else
-	    if( (ch=fgetc(arq))!= EOF ){
-	    	if((112=ch)&&(i==0)){
-	    		lexico(arq,ch2,ch,I);
-			}
+	    do{
+	    	if(i==0){
+	    		printf("|");
+	    		ch=fgetc(arq);
+	    	}
 			if(ch == '\n')
-			  	printf("\t |%i| ", ++num);
-			//putchar(ch);
-			printf("%c", ch);
+				printf("| \t linha: |%d|",(il++)+1);
+			if(ch == '\n')
+				printf("%c|", ch);
+			else{
+				printf("%c", ch);
+			}
 			ch2[i++] = ch;
 			ch2 = realloc(ch2, i+1);
-			lexico(arq,ch2,ch);
+		}while((ch=fgetc(arq))!= EOF );
+		if(ch==EOF){
+			printf("|\t linha: |%d|", il);
 		}
-		/*while( j<i ){
-			printf("%c", ch2[j++]);
-		}*/
+		//for(j=0;j<i;j++){
+		//	printf("%c", ch2[j]);
+		//}
+	
+		c= ch2;
 		
 		//printf("Existem %d linhas no arquivo\n", num);
-	return 0;
+	//return ch2;
 }
